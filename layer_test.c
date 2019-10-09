7e70e3cd16afee1cf81b05311a10614fa0d791d9
shtsno24 <shtsno.24@gmail.com>
2019-10-09 17:45:13 +0900

    UpSampling layer passed the test!

--- layer_test.c
+++ layer_test.c
@@ -3,15 +3,18 @@
 
 #include "array_printf.h"
 #include "layers/padding2d.h"
+#include "layers/up_sampling2d.h"
 
 #define array_size_h 4
-#define array_size_w 2
+#define array_size_w 4
 #define pad_h 1
 #define pad_w 1
 #define depth 3
+#define kernel 2
 
 int16_t input_array[depth][array_size_h][array_size_w];
-int16_t output_array[depth][array_size_h + 2 * pad_h][array_size_w + 2 * pad_w];
+// int16_t output_array[depth][array_size_h + 2 * pad_h][array_size_w + 2 * pad_w];
+int16_t output_array[depth][array_size_h * 2][array_size_w * 2];
 
 void padding2d_test(uint16_t padding_height, uint16_t padding_width,
 uint16_t input_depth, uint16_t input_height, uint16_t input_width, int16_t input[input_depth][input_height][input_width],
@@ -21,16 +24,50 @@ uint16_t output_height, uint16_t output_width, int16_t output[input_depth][outpu
             input_depth, input_height, input_width, input,
             output_height, output_width, output);
     
-    array_printf_3D(input_depth, input_height, input_width, input_array);
+    array_printf_3D(input_depth, input_height, input_width, input);
     printf("\r\n\r\n");
-    array_printf_3D(input_depth, output_height, output_width, output_array);
+    array_printf_3D(input_depth, output_height, output_width, output);
+    printf("\r\n\r\n");
+}
+
+void up_sampling2d_test(uint16_t kernel_size,
+uint16_t input_depth, uint16_t input_height, uint16_t input_width, int16_t input[input_depth][input_height][input_width],
+uint16_t output_depth, uint16_t output_height, uint16_t output_width, int16_t output[output_depth][output_height][output_width]){
+    up_sampling2d(kernel_size,
+    input_depth, input_height, input_width, input,
+    output_depth, output_height, output_width, output);
+
+    array_printf_3D(input_depth, input_height, input_width, input);
+    printf("\r\n\r\n");
+    array_printf_3D(input_depth, output_height, output_width, output);
     printf("\r\n\r\n");
 }
 
 int main(void){
-    padding2d_test(pad_h, pad_w,
+
+    for(int i = 0; i < depth; i++){
+        for(int j = 0; j < array_size_h; j++){
+            for(int k = 0; k < array_size_w; k++){
+                input_array[i][j][k] = (j + 1) * (k + 1);
+            }
+        }
+    }
+
+    for(int i = 0; i < depth; i++){
+        for(int j = 0; j < array_size_h + 2 * pad_h; j++){
+            for(int k = 0; k < array_size_w + 2 * pad_w; k++){
+                output_array[i][j][k] = j + 1;
+            }
+        }
+    }
+
+    // padding2d_test(pad_h, pad_w,
+    // depth, array_size_h, array_size_w, input_array,
+    // array_size_h + 2 * pad_h, array_size_w + 2 * pad_w, output_array);
+
+    up_sampling2d_test(kernel,
     depth, array_size_h, array_size_w, input_array,
-    array_size_h + 2 * pad_h, array_size_w + 2 * pad_w, output_array);
+    depth, array_size_h * 2, array_size_w * 2, output_array);
 
     return(0);
 }
