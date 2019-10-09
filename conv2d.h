2d4500eae8eaa21d759a73ed566826ea9e1afab1
shtsno24 <shtsno.24@gmail.com>
2019-10-09 11:22:42 +0900

    Bug Fix!

--- conv2d.h
+++ conv2d.h
@@ -1,8 +1,3 @@
-
-typedef struct{
-    uint16_t height, width, depth;
-}ARRAY_PARAMS_3D;
-
 uint8_t conv2d(uint16_t input_depth, uint16_t input_height, uint16_t input_width, int16_t input[input_depth][input_height][input_width],
 uint16_t output_depth, uint16_t output_height, uint16_t output_width, int16_t output[output_depth][output_height][output_width],
 int16_t bias[output_depth],
--- padding2d.h
+++ padding2d.h
@@ -1,3 +1,5 @@
-uint8_t padding2d(uint16_t padding,
+#include <stdint.h>
+
+uint8_t padding2d(uint16_t padding_height, uint16_t padding_width,
 uint16_t input_depth, uint16_t input_height, uint16_t input_width, int16_t input[input_depth][input_height][input_width],
 uint16_t output_height, uint16_t output_width, int16_t output[input_depth][output_height][output_width]);
