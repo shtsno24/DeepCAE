97c0009d91d8ef7699362084626eae4ab8137254
shtsno24 <shtsno.24@gmail.com>
2019-10-15 23:34:44 +0900

    refactor the code

--- arrays/arrays_float32.h
+++ arrays/arrays_float32.h
@@ -1,44 +1,44 @@
-/*
- * author : shtsno24
- * Date : 2019-10-15 14:53:47.645173
- *
- */
-#pragma once
-#include <stdint.h>
-
-uint16_t Padding2D_0_depth = 1, Padding2D_0_height = 30, Padding2D_0_width = 30;
-float Padding2D_0_array[1][30][30];
-
-uint16_t Conv2D_0_depth = 16, Conv2D_0_height = 28, Conv2D_0_width = 28;
-float Conv2D_0_array[16][28][28];
-
-uint16_t MaxPooling2D_0_depth = 16, MaxPooling2D_0_height = 14, MaxPooling2D_0_width = 14;
-float MaxPooling2D_0_array[16][14][14];
-
-uint16_t Padding2D_1_depth = 16, Padding2D_1_height = 16, Padding2D_1_width = 16;
-float Padding2D_1_array[16][16][16];
-
-uint16_t Conv2D_1_depth = 8, Conv2D_1_height = 14, Conv2D_1_width = 14;
-float Conv2D_1_array[8][14][14];
-
-uint16_t MaxPooling2D_1_depth = 8, MaxPooling2D_1_height = 7, MaxPooling2D_1_width = 7;
-float MaxPooling2D_1_array[8][7][7];
-
-uint16_t UpSampling2D_0_depth = 8, UpSampling2D_0_height = 14, UpSampling2D_0_width = 14;
-float UpSampling2D_0_array[8][14][14];
-
-uint16_t Padding2D_2_depth = 8, Padding2D_2_height = 16, Padding2D_2_width = 16;
-float Padding2D_2_array[8][16][16];
-
-uint16_t Conv2D_2_depth = 16, Conv2D_2_height = 14, Conv2D_2_width = 14;
-float Conv2D_2_array[16][14][14];
-
-uint16_t UpSampling2D_1_depth = 16, UpSampling2D_1_height = 28, UpSampling2D_1_width = 28;
-float UpSampling2D_1_array[16][28][28];
-
-uint16_t Padding2D_3_depth = 16, Padding2D_3_height = 30, Padding2D_3_width = 30;
-float Padding2D_3_array[16][30][30];
-
-uint16_t Conv2D_3_depth = 1, Conv2D_3_height = 28, Conv2D_3_width = 28;
-float Conv2D_3_array[1][28][28];
-
+/*
+ * author : shtsno24
+ * Date : 2019-10-15 23:34:13.019408
+ *
+ */
+#pragma once
+#include <stdint.h>
+
+uint16_t Padding2D_0_depth = 1, Padding2D_0_height = 30, Padding2D_0_width = 30;
+float Padding2D_0_array[1][30][30];
+
+uint16_t Conv2D_0_depth = 16, Conv2D_0_height = 28, Conv2D_0_width = 28;
+float Conv2D_0_array[16][28][28];
+
+uint16_t MaxPooling2D_0_depth = 16, MaxPooling2D_0_height = 14, MaxPooling2D_0_width = 14;
+float MaxPooling2D_0_array[16][14][14];
+
+uint16_t Padding2D_1_depth = 16, Padding2D_1_height = 16, Padding2D_1_width = 16;
+float Padding2D_1_array[16][16][16];
+
+uint16_t Conv2D_1_depth = 8, Conv2D_1_height = 14, Conv2D_1_width = 14;
+float Conv2D_1_array[8][14][14];
+
+uint16_t MaxPooling2D_1_depth = 8, MaxPooling2D_1_height = 7, MaxPooling2D_1_width = 7;
+float MaxPooling2D_1_array[8][7][7];
+
+uint16_t UpSampling2D_0_depth = 8, UpSampling2D_0_height = 14, UpSampling2D_0_width = 14;
+float UpSampling2D_0_array[8][14][14];
+
+uint16_t Padding2D_2_depth = 8, Padding2D_2_height = 16, Padding2D_2_width = 16;
+float Padding2D_2_array[8][16][16];
+
+uint16_t Conv2D_2_depth = 16, Conv2D_2_height = 14, Conv2D_2_width = 14;
+float Conv2D_2_array[16][14][14];
+
+uint16_t UpSampling2D_1_depth = 16, UpSampling2D_1_height = 28, UpSampling2D_1_width = 28;
+float UpSampling2D_1_array[16][28][28];
+
+uint16_t Padding2D_3_depth = 16, Padding2D_3_height = 30, Padding2D_3_width = 30;
+float Padding2D_3_array[16][30][30];
+
+uint16_t Conv2D_3_depth = 1, Conv2D_3_height = 28, Conv2D_3_width = 28;
+float Conv2D_3_array[1][28][28];
+
--- keras_array_generator.py
+++ keras_array_generator.py
@@ -11,7 +11,7 @@ output_file_fix16 = "./arrays/arrays_fix16.h"
 
 with open("keras_mnist_DCAE/keras_mnist_DCAE.json") as jfile:
     if os.path.isdir("./arrays") is False:
-            os.mkdir("./arrays")
+        os.mkdir("./arrays")
 
     model = load_model("keras_mnist_DCAE/keras_mnist_DCAE.h5")
     model.summary()
@@ -20,14 +20,15 @@ with open("keras_mnist_DCAE/keras_mnist_DCAE.json") as jfile:
 
     arrays_fix16 = []
     arrays_float32 = []
+    layers_fix16 = []
+    layers_float32 = []
 
-    itr_counter = {"MaxPooling2D" : 0, "UpSampling2D" : 0, "Conv2D" : 0, "Padding2D" : 0}
+    itr_counter = {"MaxPooling2D": 0, "UpSampling2D": 0, "Conv2D": 0, "Padding2D": 0}
     array_shapes = np.array([0, 0, 0], dtype=np.uint16)  # depth height width
 
     for layers in model_arrays_itr["config"]["layers"]:
 
         layer_name = str(layers["class_name"] + "_{}").format(itr_counter[layers["class_name"]])
-        # print(layer_name)
 
         if layers["class_name"].find("MaxPooling2D") != -1:
             size = (layers["config"]["pool_size"])[:]
@@ -57,16 +58,10 @@ with open("keras_mnist_DCAE/keras_mnist_DCAE.json") as jfile:
 
                 # generate padding layers
                 # write to float32.c file
-
-                # print(str("uint16_t Padding2D_{}_depth = {}, Padding2D_{}_height = {}, Padding2D_{}_width = {};").format(itr_counter["Padding2D"], array_shapes[0], itr_counter["Padding2D"], array_shapes[1], itr_counter["Padding2D"], array_shapes[2]))
-                # print(str("float " + layer_name + "_array[{}][{}][{}];\r\n").format(array_shapes[0], out_shapes_height + padding[0], out_shapes_height + padding[1]))
                 arrays_float32.append(str("uint16_t Padding2D_{}_depth = {}, Padding2D_{}_height = {}, Padding2D_{}_width = {};\r\n").format(itr_counter["Padding2D"], input_depth, itr_counter["Padding2D"], int(out_shapes_height + padding[0]), itr_counter["Padding2D"], int(out_shapes_height + padding[1])))
                 arrays_float32.append(str("float Padding2D_{}_array[{}][{}][{}];\r\n\r\n").format(itr_counter["Padding2D"], input_depth, int(out_shapes_height + padding[0]), int(out_shapes_height + padding[1])))
 
                 # write to fix16.c file
-
-                # print(str("uint16_t Padding2D_{}_depth = {}, Padding2D_{}_height = {}, Padding2D_{}_width = {};").format(itr_counter["Padding2D"], array_shapes[0], itr_counter["Padding2D"], array_shapes[1], itr_counter["Padding2D"], array_shapes[2]))
-                # print(str("int16_t " + layer_name + "_array[{}][{}][{}];\r\n").format(array_shapes[0], out_shapes_height + padding[0], out_shapes_height + padding[1]))
                 arrays_fix16.append(str("uint16_t Padding2D_{}_depth = {}, Padding2D_{}_height = {}, Padding2D_{}_width = {};\r\n").format(itr_counter["Padding2D"], input_depth, itr_counter["Padding2D"], int(out_shapes_height + padding[0]), itr_counter["Padding2D"], int(out_shapes_height + padding[1])))
                 arrays_fix16.append(str("int16_t Padding2D_{}_array[{}][{}][{}];\r\n\r\n").format(itr_counter["Padding2D"], input_depth, int(out_shapes_height + padding[0]), int(out_shapes_height + padding[1])))
 
@@ -84,27 +79,15 @@ with open("keras_mnist_DCAE/keras_mnist_DCAE.json") as jfile:
             print("This Layer is not available OTL")
 
         # write to float32.c file
-
-        # print(str("uint16_t " + layer_name + "_depth = {}, " + layer_name + "_height = {}, " + layer_name + "_width = {};").format(array_shapes[0], array_shapes[1], array_shapes[2]))
-        # print(str("float " + layer_name + "_array[{}][{}][{}];\r\n").format(array_shapes[0], array_shapes[1], array_shapes[2]))
         arrays_float32.append(str("uint16_t " + layer_name + "_depth = {}, " + layer_name + "_height = {}, " + layer_name + "_width = {};\r\n").format(array_shapes[0], array_shapes[1], array_shapes[2]))
         arrays_float32.append(str("float " + layer_name + "_array[{}][{}][{}];\r\n\r\n").format(array_shapes[0], array_shapes[1], array_shapes[2]))
 
         # write to fix16.c file
-
-        # print(str("uint16_t " + layer_name + "_depth = {}, " + layer_name + "_height = {}, " + layer_name + "_width = {};").format(array_shapes[0], array_shapes[1], array_shapes[2]))
-        # print(str("int16_t " + layer_name + "_array[{}][{}][{}];\r\n").format(array_shapes[0], array_shapes[1], array_shapes[2]))
         arrays_fix16.append(str("uint16_t " + layer_name + "_depth = {}, " + layer_name + "_height = {}, " + layer_name + "_width = {};\r\n").format(array_shapes[0], array_shapes[1], array_shapes[2]))
         arrays_fix16.append(str("int16_t " + layer_name + "_array[{}][{}][{}];\r\n\r\n").format(array_shapes[0], array_shapes[1], array_shapes[2]))
 
-        # print(itr_counter)
-        # print(array_shapes.dtype, array_shapes.shape, array_shapes)
-        # params_header_name_float.append("weights/" + layers["class_name"] + "_" + str(int(itr_counter / 2) - 1) + ".h")
-        # params_header_name_fix.append("weights/" + layers["class_name"] + "_" + str(int(itr_counter / 2) - 1) + "_fixed16.h")
-    # print(arrays_fix16)
-    # print(arrays_float32)
+
     with open(output_file_float32, "w") as f:
-        # headers
         todaytime = str(datetime.datetime.today())
         f.write("/*\r\n")
         f.write(" * author : shtsno24\r\n")
@@ -113,10 +96,9 @@ with open("keras_mnist_DCAE/keras_mnist_DCAE.json") as jfile:
         f.write(" */\r\n")
         f.write("#pragma once\r\n")
         f.write("#include <stdint.h>\r\n\r\n")
-        # f.write("int main(void){\r\n")
         for i in arrays_float32:
             f.write(i)
-        # f.write("}\r\n")
+
     with open(output_file_fix16, "w") as f:
         f.write("/*\r\n")
         f.write(" * author : shtsno24\r\n")
@@ -125,7 +107,5 @@ with open("keras_mnist_DCAE/keras_mnist_DCAE.json") as jfile:
         f.write(" */\r\n")
         f.write("#pragma once\r\n")
         f.write("#include <stdint.h>\r\n\r\n")
-        # f.write("int main(void){\r\n")
         for i in arrays_fix16:
             f.write(i)
-        # f.write("}\r\n")
