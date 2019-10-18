b8ab3deda897fdeab8a9ea59c4aa0abcc94fdfb4
masudalab <masudalab>
2019-10-18 16:33:53 +0900

    To support C++11,  add some scripts in array_gen

--- keras_array_generator.py
+++ keras_array_generator.py
@@ -16,10 +16,11 @@ output_files = ["template", "arrays"]
 output_precision = ["fix16", "float32"]
 
 with open("keras_mnist_DCAE/keras_mnist_DCAE.json") as jfile:
-    
-    for lang in output_languages:
-        if os.path.isdir("./arrays_" + lang) is False:
-            os.mkdir("./arrays_" + lang)
+
+    for files in output_files:
+        for lang in output_languages:
+            if os.path.isdir(str("./{}_{}").format(files, lang)) is False:
+                os.mkdir(str("./{}_{}").format(files, lang))
 
     model = load_model("keras_mnist_DCAE/keras_mnist_DCAE.h5")
     model.summary()
@@ -258,6 +259,72 @@ with open("keras_mnist_DCAE/keras_mnist_DCAE.json") as jfile:
         f.write('\tfp = fopen("template_output_float32.tsv", "w");\n\t')
         f.write(str("array_fprintf_2D_float32({0}_height, {0}_width, {0}_array[0], '\\t', fp);\n\t").format(i_old["layer_name"]))
         f.write("fclose(fp);\n\n")
+        
+        f.write("\treturn(0);\n")
+        f.write("}\n")
+
+with open("./template_cpp/template_fix16.cpp", "w") as f:
+        f.write("/*\n")
+        f.write(" * author : shtsno24\n")
+        f.write(" * Date : " + todaytime + "\n")
+        f.write(" *\n")
+        f.write(" */\n")
+        f.write("#include <cstdint>\n")
+        f.write("#include <vector>\n\n")
+
+        f.write('#include "test_data/test_data.h"\n')
+        f.write('#include "layers_cpp/array_printf_fix16.h"\n')
+        f.write('#include "arrays_cpp/arrays_fix16.h"\n')
+        f.write('#include "layers_cpp/layers.h"\n')
+        f.write('#include "weights/weights_fix16.h"\n\n')
+        f.write('using namespace std;\n\n')
+
+
+        for i in layer_params:
+            print(i)
+            if i["layer_name"].find("input") != -1:
+                f.write(str("int network(int16_t input_data[{0}*{1}*{2}], int16_t output_data[{0}*{1}*{2}]){{\n").format(i["depth"], i["height"], i["width"]))
+                f.write(str("\tuint16_t " + i["layer_name"] + "_depth = {}, " + i["layer_name"] + "_height = {}, " + i["layer_name"] + "_width = {};\n").format(i["depth"], i["height"], i["width"]))
+                f.write(str("\tvector< vector< vector< int16_t> > > {0}_array({0}_depth, vector< vector < int16_t> >({0}_height, vector< int16_t>({0}_width)));\n\n").format(i["layer_name"]))
+
+                f.write("\tfor(int depth = 0; depth < {0}_depth; depth++){{\n".format(i["layer_name"]))
+                f.write("\t\tfor(int height = 0; height < {0}_height; height++){{\n".format(i["layer_name"]))
+                f.write("\t\t\tfor(int width = 0; width < {0}_width; width++){{\n".format(i["layer_name"]))
+                f.write("\t\t\t\t{0}_array[depth][height][width] = input_data[depth * {0}_height * {0}_width + height * {0}_width + width];\n".format(i["layer_name"]))
+                f.write("\t\t\t}\n")
+                f.write("\t\t}\n")
+                f.write("\t}\n")
+
+                f.write('\tofstream fp("template_input_fix16.tsv");\n\t')
+                f.write("array_fprintf_2D_fix16(input_0_height, input_0_width, input_0_array[0], '\\t', fp, fractal_width_Conv2D_0);\n\t")
+                f.write("fp.close();\n\n")
+            elif i["layer_name"].find("Padding2D") != -1:
+                f.write(str("\tpadding2d_fix16({}, {},\n\t").format(i["padding_h"], i["padding_w"]))
+                f.write(str("{0}_depth ,{0}_height ,{0}_width ,{0}_array,\n\t").format(i_old["layer_name"]))
+                f.write(str("{0}_height ,{0}_width ,{0}_array);\n\n").format(i["layer_name"]))
+            elif i["layer_name"].find("MaxPooling2D") != -1:
+                f.write(str("\tmax_pooling2d_fix16({},\n\t").format(i["ksize_h"]))
+                f.write(str("{0}_depth ,{0}_height ,{0}_width ,{0}_array,\n\t").format(i_old["layer_name"]))
+                f.write(str("{0}_depth ,{0}_height ,{0}_width ,{0}_array);\n\n").format(i["layer_name"]))
+            elif i["layer_name"].find("UpSampling2D") != -1:
+                f.write(str("\tup_sampling2d_fix16({},\n\t").format(i["ksize_h"]))
+                f.write(str("{0}_depth ,{0}_height ,{0}_width ,{0}_array,\n\t").format(i_old["layer_name"]))
+                f.write(str("{0}_depth ,{0}_height ,{0}_width ,{0}_array);\n\n").format(i["layer_name"]))
+            elif i["layer_name"].find("Conv2D") != -1:
+                if i["activation"] == "relu":
+                    relu_flag = 1
+                else:
+                    relu_flag = 0
+                f.write(str("\tconv2d_fix16({0}_depth ,{0}_height ,{0}_width ,{0}_array,\n\t").format(i_old["layer_name"]))
+                f.write(str("{0}_depth ,{0}_height ,{0}_width ,{0}_array,\n\t").format(i["layer_name"]))
+                f.write(str("{}_b,\n\t").format(i["layer_name"]))
+                f.write(str("{0}, {1}, {2}_w, {3}, fractal_width_{2});\n\n").format(i["ksize_h"], i["ksize_w"], i["layer_name"], relu_flag))
+            
+            i_old = i.copy()
+
+        f.write('\tfp.open("template_output_fix16.tsv");\n\t')
+        f.write(str("array_fprintf_2D_fix16({0}_height, {0}_width, {0}_array[0], '\t', fp, fractal_width_{0});\n\t").format(i_old["layer_name"]))
+        f.write("fp.close();\n\n")
         
         f.write("\treturn(0);\n")
         f.write("}\n")
