25d10e3c08f64e56a3243e466f2fbcfe182ab368
shtsno24 <shtsno.24@gmail.com>
2019-11-02 22:27:28 +0900

    wip : refactor padding2d

--- layers_c/padding2d.c
+++ layers_c/padding2d.c
@@ -7,50 +7,67 @@ uint8_t padding2d_fix16(uint16_t padding_height, uint16_t padding_width,
 		int16_t* input, uint16_t output_height, uint16_t output_width,
 		int16_t* output) {
 
-	for (uint16_t depth = 0; depth < input_depth; depth++) {
-		for (uint16_t height = 0; height < padding_height; height++) {
-			for (uint16_t width = 0; width < padding_width * 2 + input_width;
-					width++) {
-				// output[depth][height][width] = 0;
-				output[depth * output_height * output_width
-						+ height * output_width + width] = 0;
-			}
+	// for (uint16_t depth = 0; depth < input_depth; depth++) {
+	// 	for (uint16_t height = 0; height < padding_height; height++) {
+	// 		for (uint16_t width = 0; width < padding_width * 2 + input_width;
+	// 				width++) {
+	// 			output[depth * output_height * output_width
+	// 					+ height * output_width + width] = 0;
+	// 		}
+	// 	}
+
+	// 	for (uint16_t height = padding_height;
+	// 			height < padding_height + input_height; height++) {
+	// 		for (uint16_t width = 0; width < padding_width; width++) {
+	// 			output[depth * output_height * output_width
+	// 					+ height * output_width + width] = 0;
+	// 		}
+	// 		for (uint16_t width = padding_width;
+	// 				width < padding_width + input_width; width++) {
+	// 			output[depth * output_height * output_width
+	// 					+ height * output_width + width] = input[depth
+	// 					* input_height * input_width
+	// 					+ (height - padding_height) * input_width
+	// 					+ (width - padding_width)];
+	// 		}
+	// 		for (uint16_t width = padding_width + input_width;
+	// 				width < 2 * padding_width + input_width; width++) {
+	// 			output[depth * output_height * output_width
+	// 					+ height * output_width + width] = 0;
+	// 		}
+	// 	}
+
+	// 	for (uint16_t height = padding_height + input_height;
+	// 			height < 2 * padding_height + input_height; height++) {
+	// 		for (uint16_t width = 0; width < padding_width * 2 + input_width;
+	// 				width++) {
+	// 			// output[depth][height][width] = 0;
+	// 			output[depth * output_height * output_width
+	// 					+ height * output_width + width] = 0;
+	// 		}
+	// 	}
+	// }
+
+	uint16_t o_slice_length = (2 * padding_height + input_height) * (2 * padding_width + input_width);
+	uint16_t i_slice_length = input_height * input_width;
+	uint16_t o_count = 0, i_count = 0;
+
+	for(uint16_t depth = 0; depth < input_depth; depth++){
+		for(uint16_t i = 0; i < padding_height * (padding_height * 2 + input_width); i++){
+			output[o_count] = 0;
+			o_count += 1;
 		}
 
-		for (uint16_t height = padding_height;
-				height < padding_height + input_height; height++) {
-//#pragma HLS unroll factor = 2
-			for (uint16_t width = 0; width < padding_width; width++) {
-				output[depth * output_height * output_width
-						+ height * output_width + width] = 0;
-			}
-			for (uint16_t width = padding_width;
-					width < padding_width + input_width; width++) {
-//#pragma HLS unroll factor = 8
-				output[depth * output_height * output_width
-						+ height * output_width + width] = input[depth
-						* input_height * input_width
-						+ (height - padding_height) * input_width
-						+ (width - padding_width)];
-			}
-			for (uint16_t width = padding_width + input_width;
-					width < 2 * padding_width + input_width; width++) {
-				output[depth * output_height * output_width
-						+ height * output_width + width] = 0;
+		for(uint16_t height = 0; height < input_height; height++){
+			for(uint16_t width = 0; width < input_width; width++){
+				output[o_count] = input[i_count];
+				i_count += 1;
+				o_count += 1;
 			}
 		}
+		
 
-		for (uint16_t height = padding_height + input_height;
-				height < 2 * padding_height + input_height; height++) {
-			for (uint16_t width = 0; width < padding_width * 2 + input_width;
-					width++) {
-				// output[depth][height][width] = 0;
-				output[depth * output_height * output_width
-						+ height * output_width + width] = 0;
-			}
-		}
 	}
-
 	return (0);
 }
 
