a274496605bb56c171e945829bf1d31cd9a8b55e
shtsno24 <shtsno.24@gmail.com>
2020-01-09 10:11:52 +0900

    にゃーん

--- tsv2img.py
+++ tsv2img.py
@@ -4,8 +4,9 @@ from PIL import Image
 tsv_file = input("file name : ")
 tsv_array = np.loadtxt(tsv_file + ".tsv", dtype=np.float32, delimiter='\t')
 print(tsv_array.shape, tsv_array.dtype)
-
+print("max : {}\nmin : {}".format(np.max(tsv_array), np.min(tsv_array)))
 tsv_array = (tsv_array - np.min(tsv_array)) / (np.max(tsv_array) - np.min(tsv_array)) * 255
 
 pil_img = Image.fromarray(tsv_array.astype(np.uint8))
 pil_img.save(tsv_file + ".png")
+input(">>>")
